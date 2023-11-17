0ea347f3544765278106126548c0516c69a1ba30
2000923 <2000923@unmsm.edu.pe>
2023-11-17 09:38:05 -0500

    update repository ansible-role-devops-setup

--- ansible-role-devops-setup/files/install_prerequisites.sh
+++ ansible-role-devops-setup/files/install_prerequisites.sh
@@ -2,9 +2,12 @@
 #
 # packages and install
 SSH_HOME="~/.ssh"
-sudo apt install python3 python3-pip curl git ssh sshpass -y
+sudo apt update --force-yes -y
+sudo apt install python3 python3-pip curl git ssh sshpass -y --force-yes
 sudo pip3 install ansible-core
-mkdir ~/.ssh && chmod 755 ~/.ssh
+if [[ ! -d ${SSH_HOME} ]]; then
+  mkdir ~/.ssh && chmod 755 ~/.ssh
+fi
 ssh-keygen -t rsa -b 2048 -N "" -f ~/.ssh/$(whoami)/id_rsa
 cat ${SSH_HOME}/id_rsa.pub > ~/.ssh/authorized_keys
 ansible-galaxy collection install community.general
